0ddb6cda26610f5b660ae1d90eb24dbafb7ac7a0
Zoran <znikic@gmail.com>
2023-08-16 19:21:43 +0200

    change 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(this.getMessage());
+        System.out.println(getMessage());
     }
 
     public String getMessage() {
